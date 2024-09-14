1e7817e72c07bac7a379741cd3b909ed118a0acb
GianfrancoPuca <gianfrancopuca@hotmail.it>
2024-09-14 13:44:30 +0200

    GL/MN/CS updated

--- js/script.js
+++ js/script.js
@@ -1,10 +1,7 @@
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
 function myFunction() {
   document.getElementById("myDropdown").classList.toggle("show");
 }
 
-// Close the dropdown menu if the user clicks outside of it
 window.onclick = function(event) {
   if (!event.target.matches('.dropbtn')) {
     var dropdowns = document.getElementsByClassName("dropdown-content");
@@ -23,3 +20,6 @@ window.onscroll = function() {
       footer.style.display = 'block';
   }
 }; 
+document.getElementById('scopri-btn').addEventListener('click', function() {
+  window.location.href = 'scopri.html';
+});
